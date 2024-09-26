3259b3ee431e495bd38a67d35aab63584dd0dd89
Touqeer Hassan <touqeerhassan76@gmail.com>
2024-09-26 13:14:12 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    TELEGRAM_API_ID: process.env.TELEGRAM_API_ID,
-    TELEGRAM_API_HASH: process.env.TELEGRAM_API_HASH,
-    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-  },
-  images: {
-    domains: ["api.telegram.org", "t.me"],
-  },
+  // reactStrictMode: true,
+  // env: {
+  //   TELEGRAM_API_ID: process.env.TELEGRAM_API_ID,
+  //   TELEGRAM_API_HASH: process.env.TELEGRAM_API_HASH,
+  //   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+  // },
+  // images: {
+  //   domains: ["api.telegram.org", "t.me"],
+  // },
 };
 
 export default nextConfig;
